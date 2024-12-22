ac59317ab854eeb12f94806d1cd6efde618611d3
John Vu <hoanganh170788@gmail.com>
2024-12-22 19:53:22 +0700

    Update secureboot.sh

--- secureboot.sh
+++ secureboot.sh
@@ -10,11 +10,15 @@ openssl req -new -x509 -newkey rsa:2048 -keyout $PATH/nvidia.key -outform DER -o
 echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf
 sudo update-initramfs -u
 
-# Enroll with mokutil
-echo "You will be prompt to enter passphrase (Not necessarily your password)"
+## Enroll with mokutil
+echo "You will be prompt to enter passphrase (Not necessarily your password)"; sleep 2
 sudo mokutil --import $PATH/nvidia.key
 
 # To-do: Put nvidia.sh to startup for automatically installation
 
+CRONTAB_ENTRY="@reboot ~/nvidia-driver-autoscript/nvidia.sh"
+(crontab -l 2>/dev/null; echo "$CRONTAB_ENTRY") | crontab -
+
+# Reboot for MOK key enrollment
 sudo reboot
 
