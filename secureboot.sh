eb9ef2bde583cce5ba0e154eacd7402c60e5b573
John Vu <hoanganh170788@gmail.com>
2024-12-24 10:50:52 +0700

    User input before installing supergfxctl

--- secureboot.sh
+++ secureboot.sh
@@ -4,6 +4,7 @@
 mkdir /home/$USER/tempnvd
 cd /home/$USER/tempnvd
 DIR_PATH="/home/$USER/tempnvd"
+sudo apt update
 openssl req -new -x509 -newkey rsa:2048 -keyout $DIR_PATH/nvidia.key -outform DER -out $DIR_PATH/nvidia.der -nodes -days 36500 -subj "/CN=Graphics Drivers"
 
 ## Disable nouveau
@@ -13,15 +14,15 @@ fi
 sudo update-initramfs -u
 
 ## Enroll with mokutil
-echo "You will be prompt to enter passphrase (Not necessarily your password)"; sleep 2
+echo "You will be prompt to enter passphrase (Only used to enroll key)"; sleep 2
 sudo mokutil --import $DIR_PATH/nvidia.der
 
 # To-do: Put nvidia.sh to startup for automatically installation
 
-CRONTAB_ENTRY="@reboot ~/nvidia-driver-autoscript/nvidia.sh"
-(crontab -l 2>/dev/null; echo "$CRONTAB_ENTRY") | crontab -
+# CRONTAB_ENTRY="@reboot ~/nvidia-driver-autoscript/nvidia.sh"
+# (crontab -l 2>/dev/null; echo "$CRONTAB_ENTRY") | crontab -
+# Not working for some reasons. Looking for alternative. Manually run nvidia.sh
 
 # Reboot for MOK key enrollment
-echo "Reboot!"
-#sudo reboot
+sudo reboot
 
