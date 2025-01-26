9e9d5b68d0467b51d65df2c27b2b5047ef97925d
John Vu <hoanganh170788@gmail.com>
2025-01-26 16:15:52 +0700

    Structure changes.

--- nvidia.sh
+++ nvidia.sh
@@ -1,7 +1,53 @@
 #!/bin/bash
 
 # Prequisite
-sudo apt install linux-headers-$(uname -r) pkg-config build-essential curl wget libglvnd-dev # Missing quite a few prequisite packages here
+sudo apt install linux-headers-$(uname -r) pkg-config build-essential curl wget libglvnd-dev awk
+
+installNvidiaDriver(){
+	sudo sh ./*.run --module-signing-secret-key=/home/$USER/tempnvd/nvidia.key --module-signing-public-key=/home/$USER/tempnvd/nvidia.der
+
+	# TO-DO: modeset=1 fbdev=1
+
+	# Enable nvidia-resume.service
+	## Fix for graphics tearing after wake from sleep on KDE Plasma
+	sudo touch /etc/modprobe.d/nvidia-power-management.conf >> /dev/null
+	echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1 NVreg_TemporaryFilePath=/var/tmp" >> sudo tee -a /etc/modprobe.d/nvidia-power-management.conf
+	echo "options nvidia NVreg_DynamicPowerManagement=0x02" >> sudo tee -a /etc/modprobe.d/nvidia-power-management.conf
+	sudo systemctl enable nvidia-suspend.server nvidia-hibernate.service nvidia-resume.service
+
+	#nvidia-drm modeset=1 fbdev=1
+	sudo touch /etc/modprobe.d/nvidia.conf >> /dev/null
+	echo "options nvidia-drm modeset=1 fbdev=1" >> sudo tee -a /etc/modprobe.d/nvidia.conf
+	sudo update-initramfs -u
+
+
+}
+
+# supergfxctl
+installSupergfxctl(){
+	echo "Installing supergfxctl (For Optimus Laptop)" && sleep 2
+	sudo apt install libudev-dev 
+
+	## remove xorg NVIDIA-only cfg for hybrid
+	sudo rm -rf /etc/X11/xorg.conf
+
+	## Installing Rust
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	source ~/.cargo/env
+
+	## supergfxctl
+	git clone https://gitlab.com/asus-linux/supergfxctl.git
+	cd supergfxctl
+	make && sudo make install
+
+	sudo systemctl enable supergfxd.service --now
+	sudo usermod -a -G users $USER
+}
+
+getVer(){
+	versions=$(curl -s "https://download.nvidia.com/XFree86/Linux-x86_64/" | grep -oE "href='[0-9]+\.[0-9]+(?:\.[0-9]+)?\/'" | sed "s/href='\(.*\)\/'/\1/" | sort -V)
+	recommended=$(curl -s "https://download.nvidia.com/XFree86/Linux-x86_64/latest.txt" | awk '{print $1}')
+}
 
 # NVIDIA driver
 echo "Which branch? (1, 2, 3)"
@@ -71,34 +117,4 @@ case $br_option in
 		;;
 esac
 
-sudo sh ./*.run --module-signing-secret-key=/home/$USER/tempnvd/nvidia.key --module-signing-public-key=/home/$USER/tempnvd/nvidia.der
 
-# Enable nvidia-resume.service
-## Fix for graphics tearing after wake from sleep on KDE Plasma
-sudo touch /etc/modprobe.d/nvidia-power-management.conf >> /dev/null
-echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1 NVreg_TemporaryFilePath=/var/tmp" >> sudo tee -a /etc/modprobe.d/nvidia-power-management.conf
-sudo systemctl enable nvidia-suspend.server nvidia-hibernate.service nvidia-resume.service
-
-# supergfxctl
-clear
-echo "Installing supergfxctl? (For Optimus Laptop) (y/n)"
-read supergfxctl_option
-if [ "$supergfxctl_option"="y" ]; then
-	## remove xorg NVIDIA-only cfg for hybrid
-	sudo apt install libudev-dev 
-	rm -rf /etc/X11/xorg.conf
-
-	## Installing Rust
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-	source ~/.cargo/env
-
-	## supergfxctl
-	git clone https://gitlab.com/asus-linux/supergfxctl.git
-	cd supergfxctl
-	make && sudo make install
-
-	sudo systemctl enable supergfxd.service --now
-	sudo usermod -a -G users $USER
-else
-	echo "Won't install supergfxctl"
-fi
